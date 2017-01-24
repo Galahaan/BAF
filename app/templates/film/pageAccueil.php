4b42af3a66000d547666065a59586d301b68bd63
Etudiant <etudiant@wf3.fr>
2017-01-24 13:28:41 +0100

    Page d'accueil operationnelle avec liens vers les listes de films ... reste a completer la BDD pour avoir plus de 3 listes

--- app/templates/film/pageAccueil.php
+++ app/templates/film/pageAccueil.php
@@ -2,95 +2,100 @@
 
 <?php $this->start('main_content') ?>
 
-	<p> Tony, à toi de jouer !... </p>
+<div class="wraper">
 
-	<p>
-		<a href="<?= $this->url('docW') ?>">Doc sur W</a>
-	</p>
-	
-	<p>
-		<a href="<?= $this->url('pageSelections', ['theme' => "palmesOr"]) ?>">Palmes d'Or</a>
-	</p>
-
-	<p>
-		<a href="<?= $this->url('pageSelections', ['theme' => "007"]) ?>">James Bond</a>
-	</p>
-
-	<p>
-		<a href="<?= $this->url('pageSelections', ['theme' => "cesars"]) ?>">Césars</a>
-	</p>
-
-	        <div class="wraper">
-	            <div class="iso-nav">
-	                <ul>
-	                <!-- 
-	                - recompenses
-	                - ma baf
-	                - autres
-	            -->
-	            <li class="active" data-filter="*">All Items</li>
-	            <li data-filter=".genre"><img src="img/clap.png" alt=""></li>
-	            <li data-filter=".recompense"><img src="img/couronne.png" alt=""></li>
-	            <li data-filter=".perso"><img src="img/perso.png" alt=""></li>
-	            <li data-filter=".autre"><img src="img/points.png" alt=""></li>
-	        </ul>
-	    </div>
-	    <div class="main-iso">
-	        <div class="item  genre">
-
-	            <img src="img/aventure.jpg" alt="" class="taille_img">
-	            <a href="#" class="taille_a couleur_genre">
-	               <span class="affichage_text">
-	                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-	               </span>
-	               <span class="affichage_titre">
-	                    AVENTURE
-	               </span>
-	            </a>
-
-	        </div>
-
-	        <div class="item  recompense">
-
-	            <img src="img/image_1.jpg" alt="" class="taille_img">
-	            <a href="#" class="taille_a couleur_recompense">
-	               <span class="affichage_text">
-	                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-	               </span>
-	               <span class="affichage_titre">
-	                    AVENTURE
-	               </span>
-	            </a>
-
-	        </div>
-
-	        <div class="item  perso">
-
-	            <img src="img/image_1.jpg" alt="" class="taille_img">
-	            <a href="#" class="taille_a couleur_perso">
-	               <span class="affichage_text">
-	                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-	               </span>
-	               <span class="affichage_titre">
-	                    AVENTURE
-	               </span>
-	            </a>
-
-	        </div>
-
-	        <div class="item  autre">
-
-	            <img src="img/image_1.jpg" alt="" class="taille_img">
-	            <a href="#" class="taille_a couleur_autre">
-	               <span class="affichage_text">
-	                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-	               </span>
-	               <span class="affichage_titre">
-	                    AVENTURE
-	               </span>
-	            </a>
-
-	        </div>
+	<div class="iso-nav">
+		<!-- Liste des icônes de groupes pour tri rapide -->
+		<ul>
+			<li class="active" data-filter="*">All Items</li>
+			<li data-filter=".preSelections"><img src="<?= $this->assetUrl('img/accueil/preSelections.png') ?>" alt=""></li>
+			<li data-filter=".genre"><img src="<?= $this->assetUrl('img/accueil/clap.png') ?>" alt=""></li>
+			<li data-filter=".selectionsPerso"><img src="<?= $this->assetUrl('img/accueil/selectionsPerso.png') ?>" alt=""></li>
+			<li data-filter=".autre"><img src="<?= $this->assetUrl('img/accueil/points.png') ?>" alt=""></li>
+		</ul>
 	</div>
 
+	<div class="main-iso">
+
+		<!-- ///////////////////////////////////////////////////////////////////////////// -->
+		<!-- Groupe des Présélections (récompenses, classiques, cultes, UGC, CDC, etc ...) -->
+
+		<div class="item  preSelections">
+			<img src="<?= $this->assetUrl('img/accueil/palmesOr.png') ?>" alt="" class="taille_img">
+			<a href="<?= $this->url('pageSelections', ['theme' => "palmesOr"]) ?>" class="taille_a couleur_preSelections">
+				<span class="affichage_titre">
+					Palmes d'Or
+				</span>
+				<span class="affichage_text">
+					<!--  -->
+				</span>
+			</a>
+		</div>
+
+		<div class="item  preSelections">
+			<img src="<?= $this->assetUrl('img/accueil/cesars.jpg') ?>" alt="" class="taille_img">
+			<a href="<?= $this->url('pageSelections', ['theme' => "cesars"]) ?>" class="taille_a couleur_preSelections">
+				<span class="affichage_titre">
+					Césars
+				</span>
+				<span class="affichage_text">
+					<!--  -->
+				</span>
+			</a>
+		</div>
+
+		<!-- ///////////////////////////////////////////////////////////////////////////// -->
+		<!-- Groupe des Sélections personnelles (vus, à voir, préférés, ...)               -->
+
+		<div class="item  selectionsPerso">
+			<img src="<?= $this->assetUrl('img/accueil/selectionsPerso.jpg') ?>" alt="" class="taille_img">
+			<a href="#" class="taille_a couleur_selectionsPerso">
+				<span class="affichage_titre">
+					Films vus
+				</span>
+				<span class="affichage_text">
+					<!--  -->
+				</span>
+			</a>
+		</div>
+
+		<!-- ///////////////////////////////////////////////////////////////////////////// -->
+		<!-- Genres (aventure, comédie dramatique, comédie, policier, drame, etc ...)      -->
+		<div class="item  genre">
+			<img src="<?= $this->assetUrl('img/accueil/genres.jpg') ?>" alt="" class="taille_img">
+			<a href="#" class="taille_a couleur_genres">
+				<span class="affichage_titre">
+					Comédie dramatique
+				</span>
+				<span class="affichage_text">
+					<!--  -->
+				</span>
+			</a>
+		</div>
+
+		<!-- ///////////////////////////////////////////////////////////////////////////// -->
+		<!-- Autres (classiques, cultes, UGC, etc ...)                                     -->
+		<div class="item  autre">
+			<img src="<?= $this->assetUrl('img/accueil/007.png') ?>" alt="" class="taille_img">
+			<a href="<?= $this->url('pageSelections', ['theme' => "007"]) ?>" class="taille_a couleur_autre">
+				<span class="affichage_titre">
+					007
+				</span>
+				<span class="affichage_text">
+					<!--  -->
+				</span>
+			</a>
+		</div>
+
+
+	</div>
+</div>
+
 <?php $this->stop('main_content') ?>
+
+
+
+
+
+<a href="<?= $this->url('docW') ?>">Doc sur W</a>
+
--- app/templates/layoutFiles/head.php
+++ app/templates/layoutFiles/head.php
@@ -9,7 +9,7 @@
     <!--Jquery CDN -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <!-- Bootstrap Core CSS -->
-    <link href="<?= $this->assetUrl('css/bootstrap.min.css') ?>" rel="stylesheet">
+    <link rel="stylesheet" href="<?= $this->assetUrl('css/bootstrap.min.css') ?>">
     <!-- Custom CSS -->
-    <link href="<?= $this->assetUrl('css/bootstrap.css') ?>" rel="stylesheet">
-    
+    <link rel="stylesheet" href="<?= $this->assetUrl('css/bootstrap.css') ?>">
+    <link rel="stylesheet" href="<?= $this->assetUrl('css/style-isotope.css') ?>">
