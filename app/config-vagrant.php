1f2d1c3286c4c25134d8eee4ed433cca8696e420
C.L.R <clr.wf3@use.startmail.com>
2017-04-12 15:51:49 +0200

    modifs suite a la mise en ligne

--- app/config-vagrant.php
+++ app/config-vagrant.php
@@ -1,5 +1,7 @@
 <?php 
 
+
+
 $w_config = [
    	//information de connexion à la bdd
 	'db_host' => 'localhost',						//hôte (ip, domaine) de la bdd
@@ -10,17 +12,17 @@ $w_config = [
 
 	//authentification, autorisation
 
-    //   a priori il faudra remplacer 'users' par 'wusers'
-    //   à cause du ... de bug de W
+    // il a fallu remplacer 'users' par 'wusers' à cause du bug de W
 
-	'security_user_table' => 'users',				//nom de la table contenant les infos des utilisateurs
+	'security_user_table' => 'wusers',				//nom de la table contenant les infos des utilisateurs
 	'security_id_property' => 'id',					//nom de la colonne pour la clef primaire
 	'security_username_property' => 'username',		//nom de la colonne pour le "pseudo"
 	'security_email_property' => 'email',			//nom de la colonne pour l'"email"
 	'security_password_property' => 'password',		//nom de la colonne pour le "mot de passe"
 	'security_role_property' => 'role',				//nom de la colonne pour le "role"
 
-	'security_login_route_name' => 'login',			//nom de la route affichant le formulaire de connexion
+	'security_login_route_name' => 'pageConnexion',	//nom de la route affichant le formulaire de connexion
+													// par défaut dans W, c'est 'login'
 ];
 
 require('routes.php');
